cd55980b7a77713d2992f0c21980c7a94519a3f3
IshaNalawade <isha.nalawade@gmail.com>
2021-03-21 20:33:35 +0530

    1st

--- Khoj.py
+++ Khoj.py
@@ -2,24 +2,28 @@ import params as params
 import pyrebase
 from flask import Flask, render_template, request
 import smtplib
+
 app = Flask(__name__, template_folder='templates')
 
 config = {
-  "apiKey": "AIzaSyD8Uy9tJWNoRUEf7zo1212rD1cA20OKb80",
-  "authDomain": "mars-c7bdc.firebaseapp.com",
-  "databaseURL": "https://mars-c7bdc-default-rtdb.firebaseio.com",
-  "projectId": "mars-c7bdc",
-  "storageBucket": "mars-c7bdc.appspot.com",
-  "messagingSenderId": "118583428832",
-  "appId": "1:118583428832:web:7643daf17eb2de5d72b285",
-  "measurementId": "G-9RK6Y1NT4L"
+    "apiKey": "AIzaSyD8Uy9tJWNoRUEf7zo1212rD1cA20OKb80",
+    "authDomain": "mars-c7bdc.firebaseapp.com",
+    "databaseURL": "https://mars-c7bdc-default-rtdb.firebaseio.com",
+    "projectId": "mars-c7bdc",
+    "storageBucket": "mars-c7bdc.appspot.com",
+    "messagingSenderId": "118583428832",
+    "appId": "1:118583428832:web:7643daf17eb2de5d72b285",
+    "measurementId": "G-9RK6Y1NT4L"
 }
-firebase=pyrebase.initialize_app(config)
-db=firebase.database()
-#comments added
-#hfhfhfhhfhf
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
-def mail_func(rec,msg):
+
+
+# comments added
+# hfhfhfhhfhf
+
+def mail_func(rec, msg):
     sender = "khoj.alerts@gmail.com"
     password = "Khoj@123"
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -29,23 +33,43 @@ def mail_func(rec,msg):
     print("done")
 
 
-@app.route("/",methods=['GET','POST'])
+@app.route("/signup", methods=['GET', 'POST'])
+def print2():
+    if request.method == 'POST' or request.method == 'GET':
+        data3 = {
+            "FullName": request.form.get('FullName'),
+            "Mobile": request.form.get('Mobile'), "Password": request.form.get('Confirm')
+        }
+
+        db.child("Users").child(str(request.form.get('Mobile'))).set(data3)
+    return render_template('signup.html', params=params)
+
+
+
+@app.route("/", methods=['GET', 'POST'])
+def print3():
+    return render_template('login.html', params=params)
+
+
+@app.route("/index", methods=['GET', 'POST'])
 def print4():
 
-    if request.method=='POST':
-             data2 = {
-                 "Email": request.form.get('email'),
-                 "MobileNo": request.form.get('MobileNo'), "DOB": request.form.get('dateofbirth'),
-                 "State": request.form.get('State'), "City": request.form.get('City'),
-                 "Pincode": request.form.get('Pincode'),"Gender":request.form.get('optradio'),
-                 "Age": request.form.get('Age'), "Height": request.form.get('Height'),
-                 "Description": request.form.get('Description')
-                 }
-             db.child("Alert").child(str(request.form.get('fname'))).set(data2)
-             mail_func(request.form.get('email'),(request.form.get('fname') + "\n" + request.form.get('MobileNo') +"\n" + request.form.get('State') + "\n" +request.form.get('optradio')))
+    if request.method == 'POST':
+        data2 = {
+            "Email": request.form.get('email'),
+            "MobileNo": request.form.get('MobileNo'), "DOB": request.form.get('dateofbirth'),
+            "State": request.form.get('stt'), "City": request.form.get('stt2'),
+            "Pincode": request.form.get('Pincode'), "Gender": request.form.get('optradio'),
+            "Age": request.form.get('Age'), "Height": request.form.get('Height'),
+            "Description": request.form.get('Description'),
+            "Image": request.form.get('file')
+        }
+
+        db.child("Alert").child(str(request.form.get('fname'))).set(data2)
+        mail_func(request.form.get('email'), (
+                    request.form.get('fname') + "\n" + request.form.get('MobileNo') + "\n" + request.form.get(
+                'stt') + "\n" + request.form.get('optradio')))
     return render_template('index.html', params=params)
-
-
 
 
 app.run(debug=True)
